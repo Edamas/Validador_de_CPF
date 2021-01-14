a3f00da7c6a1a0dd44e83037f44f775b07e9ffc8
Elysio Neto <49333427+Edamas@users.noreply.github.com>
2021-01-14 15:49:14 -0300

    Retirada a verificação de 11 dígitos
    A verificação de 11 dígitos já é garantida nas fórmulas de verificação dos dígitos verificadores, por isso, foi eliminada das condições para validação, encurtando o código.

--- valida_cpf.py
+++ valida_cpf.py
@@ -1,5 +1,11 @@
 import numpy as np
+
+
 cpf = [int(n) for n in input('Digite um CPF: ') if n.isnumeric()]
-try:	valido = [len(cpf) , 		(sum(np.multiply(np.arange(10,1,-1),		cpf[0:-2])) * 10) % 11,		(sum(np.multiply(np.arange(11,1,-1),		cpf[0:-1])) * 10) % 11] == [11, 									cpf[-2],									cpf[-1]] and len(set(cpf)) != 1
-except:	valido = False
+try:
+	valido = [(sum(np.multiply(np.arange(10,1,-1),		cpf[0:-2])) * 10) % 11,
+			  (sum(np.multiply(np.arange(11,1,-1),		cpf[0:-1])) * 10) % 11] == [cpf[-2],
+																						cpf[-1]] and len(set(cpf)) != 1
+except:
+	valido = False
 print('CPF válido: ', valido)
